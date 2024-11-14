459f2dd717acf9f5afcc59ed3e08b3b303420d6e
Shun Sakai <sorairolake@protonmail.ch>
2024-11-14 14:20:20 +0900

    chore(release): Update version to 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -66,7 +66,7 @@
 //! [C23]: https://en.cppreference.com/w/c/23
 //! [Zig]: https://ziglang.org/
 
-#![doc(html_root_url = "https://docs.rs/bit-int/0.1.0/")]
+#![doc(html_root_url = "https://docs.rs/bit-int/0.1.1/")]
 #![no_std]
 // Lint levels of rustc.
 #![deny(missing_docs)]
